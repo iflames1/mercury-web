e982d5d54e2e07f62d0ff8afeab8fbb972b92913
iflames1 <isaacomenuche@gmail.com>
2025-01-07 21:51:02 +0100

    feat: implemented Import wallet using secret key

--- src/app/_components/login.tsx
+++ src/app/_components/login.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import TermsCondition from "./terms-and-conditions";
 import { Button } from "@/components/ui/button";
 import GenerateWallet from "./generate-wallet";
+import ImportWallet from "./import-wallet";
 
 export default function Login() {
 	const [termsAccepted, setTermsAccepted] = useState(false);
@@ -12,7 +13,7 @@ export default function Login() {
 		return (
 			<div className="grid gap-4 sm:grid-cols-2">
 				<GenerateWallet />
-				<Button variant={"secondary"}>Import Wallet</Button>
+				<ImportWallet />
 			</div>
 		);
 	}
